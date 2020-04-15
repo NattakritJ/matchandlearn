a814383055eedc585cefa82c690532882e6f2fe2
Nattakrit Jatupattaradit <42247051+cpre20068@users.noreply.github.com>
2020-04-15 17:49:32 +0700

    Add comment for pipeline.py

--- tinderforeduapp/pipeline.py
+++ tinderforeduapp/pipeline.py
@@ -1,27 +1,37 @@
+# pipeline is custom function use when user logged in with facebook
+
 from .models import UserInfo, ProfilePic
 from django.contrib.auth.models import User
 from social_core.pipeline.user import get_username as social_get_username
-from datetime import date, datetime
+from datetime import datetime
 user_email = ""
 
+# get email from facebook
 def get_email(backend, user, response, *args, **kwargs):
+    # set email as global variable
     global user_email
-    if response.get('email') == None:
+    # if facebook user doesn't have email associate in account, use name instead
+    if response.get('email') is None:
         user_email = (response.get('name')).split(" ")[0] + (response.get('name')).split(" ")[1]
     else:
         user_email = (response.get('email')).split("@")[0]
-
+# create user UserInfo object
 def user_profile_db(backend, user, response, *args, **kwargs):
+    # check if UserInfo object not exist
     if not User.objects.filter(email=response.get('email')).exists():
+        # receive data from facebook
         gender = ''
-        birthdate = response.get('birthday')
-        born = datetime.strptime(birthdate, '%m/%d/%Y')
+        birth_date = response.get('birthday')
+        # calculate age from current date
+        born = datetime.strptime(birth_date, '%m/%d/%Y')
         age = int((datetime.today() - born).days/365)
         if response.get('gender') == 'male':
             gender = 'Male'
         if response.get('gender') == 'female':
             gender = 'Female'
-        if response.get('email') == None:
+        # if facebook user doesn't have email associate in account
+        if response.get('email') is None:
+            # create UserInfo object for new user
             user = UserInfo.objects.create(name=(response.get('name')).split(" ")[0] + (response.get('name')).split(" ")[1],
                                     school='',
                                     age=age,
@@ -29,14 +39,17 @@ def user_profile_db(backend, user, response, *args, **kwargs):
                                     lastname=(response.get('name')).split(" ")[1],
                                     gender=gender, fb_link=response.get('link'))
         else:
+            # create UserInfo object for new user
             user = UserInfo.objects.create(name=(response.get('email')).split("@")[0],
                                 school='',
                                 age=age,
                                 firstname=(response.get('name')).split(" ")[0],
                                 lastname=(response.get('name')).split(" ")[1],
                                 gender=gender, fb_link=response.get('link'))
+        # assign default profile picture for new user
         ProfilePic.objects.create(user=user, images='default.png')
 
+# set username for new user by using email account (remove email domain)
 def get_username(strategy, details, backend, user=None, *args, **kwargs):
     result = social_get_username(strategy, details, backend, user=user, *args, **kwargs)
     result['username'] = user_email
--- tinderforeduapp/urls.py
+++ tinderforeduapp/urls.py
@@ -1,31 +1,28 @@
 from django.urls import path
-
 from . import views
-
 from django.conf.urls import url
-from django.contrib.auth.views import LoginView, LogoutView,PasswordResetView, PasswordResetDoneView, \
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, \
     PasswordResetConfirmView, PasswordResetCompleteView
 from django.conf.urls import include
 from django.views.generic import TemplateView
-
 from . import views as core_views
 
 app_name = 'tinder'
 urlpatterns = [
     path('', views.homepage, name='home'),
-    path('<int:user_id>/my_profile/', views.my_profile, name ='my_profile'),
-    path('<int:user_id>/delete_subject/', views.delete_subject, name ='delete_subject'),
+    path('<int:user_id>/my_profile/', views.my_profile, name='my_profile'),
+    path('<int:user_id>/delete_subject/', views.delete_subject, name='delete_subject'),
     path('login/', LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(next_page='/login'), name="logout"),
     url(r'^signup/$', core_views.signup, name='signup'),
-    path('<int:user_id>/profile/',views.searched_profile,name='profile'),
+    path('<int:user_id>/profile/', views.searched_profile, name='profile'),
     path('chat/', include('chat.urls')),
-    path('<int:user_id>/match/',views.match,name="match"),
-    path('<int:user_id>/unmatch/',views.unmatch,name="unmatched"),
-    path('<int:user_id>/match_request/',views.match_request,name="match_request"),
-    path('<int:user_id>/accept_request/',views.accept_request,name="accept_request"),
-    path('<int:user_id>/students_list/',views.students_list,name="students_list"),
-    path('<int:user_id>/matched_profile',views.matched_profile,name="matched_profile"),
+    path('<int:user_id>/match/', views.match, name="match"),
+    path('<int:user_id>/unmatch/', views.unmatch, name="unmatched"),
+    path('<int:user_id>/match_request/', views.match_request, name="match_request"),
+    path('<int:user_id>/accept_request/', views.accept_request, name="accept_request"),
+    path('<int:user_id>/students_list/', views.students_list, name="students_list"),
+    path('<int:user_id>/matched_profile', views.matched_profile, name="matched_profile"),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.confirmation_email_income, name='activate'),
     url(r'^password_reset/$', PasswordResetView.as_view(), name='password_reset'),
@@ -36,6 +33,6 @@ urlpatterns = [
     path('privacypolicy/', TemplateView.as_view(template_name="tinder/privacy.html"), name='privacy'),
     path('faq/', TemplateView.as_view(template_name="tinder/faq.html"), name='faq'),
     path('aboutus/', TemplateView.as_view(template_name="tinder/aboutus.html"), name='aboutus'),
-    path('fb_data/', views.facebook_additional_data_request, name ='fb_data'),
+    path('fb_data/', views.facebook_additional_data_request, name='fb_data'),
     path('<int:user_id>/edit_profile/', views.edit_profile, name="edit_profile"),
 ]
