db27ae8066da57690e49d8a59f4bc128f8abb977
Nattakrit Jatupattaradit <42247051+cpre20068@users.noreply.github.com>
2020-04-25 02:59:19 +0700

    Add functional test to test gallery feature

--- functional_test.py
+++ functional_test.py
@@ -4,14 +4,36 @@ import time
 import unittest
 
 
-class UploadImageToGallery(unittest.TestCase):
+class FunctionalTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Edge("msedgedriver.exe")
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_add_image(self):
+    def another_user_can_see_images_and_new_image_when_user_add_new_image(self):
+        # Kitsanapong login to Match and Learn website
+        self.browser.get('http://127.0.0.1:8000/login')
+        username_box = self.browser.find_element_by_id('id_username')
+        password_box = self.browser.find_element_by_id('id_password')
+        username_box.send_keys('tongu19541')
+        password_box.send_keys("1qaz2wsx3edc4rfv5tgb")
+        login_button = self.browser.find_element_by_id('login_btn')
+        login_button.click()
+        time.sleep(2)
+        # He go to student and tutor list
+        student_and_tutor_list_btn = self.browser.find_element_by_id('student_and_tutor_list')
+        student_and_tutor_list_btn.click()
+        time.sleep(2)
+        # He click on Nattakrit profile
+        tongu20068_profile_btn = self.browser.find_element_by_id('user_tongu20068')
+        tongu20068_profile_btn.click()
+        time.sleep(2)
+        # He saw 3 images from Nattakrit profile
+        self.browser.find_element_by_id('pic_1')
+        self.browser.find_element_by_id('pic_2')
+        self.browser.find_element_by_id('pic_3')
+        time.sleep(2)
         # Nattakrit login to Match and Learn website
         self.browser.get('http://127.0.0.1:8000/login')
         username_box = self.browser.find_element_by_id('id_username')
@@ -43,6 +65,29 @@ class UploadImageToGallery(unittest.TestCase):
         add_image_submit_button.click()
         time.sleep(2)
         # After that, he saw one image appear on his gallery. The total of images is four.
+        self.browser.find_element_by_id('pic_1')
+        self.browser.find_element_by_id('pic_2')
+        self.browser.find_element_by_id('pic_3')
+        self.browser.find_element_by_id('pic_4')
+        time.sleep(2)
+        # Kitsanapong login to Match and Learn website
+        self.browser.get('http://127.0.0.1:8000/login')
+        username_box = self.browser.find_element_by_id('id_username')
+        password_box = self.browser.find_element_by_id('id_password')
+        username_box.send_keys('tongu19541')
+        password_box.send_keys("1qaz2wsx3edc4rfv5tgb")
+        login_button = self.browser.find_element_by_id('login_btn')
+        login_button.click()
+        time.sleep(2)
+        # He go to student and tutor list
+        student_and_tutor_list_btn = self.browser.find_element_by_id('student_and_tutor_list')
+        student_and_tutor_list_btn.click()
+        time.sleep(2)
+        # He click on Nattakrit profile
+        tongu20068_profile_btn = self.browser.find_element_by_id('user_tongu20068')
+        tongu20068_profile_btn.click()
+        time.sleep(2)
+        # He saw 3 old images and 1 new image from Nattakrit profile
         self.browser.find_element_by_id('pic_1')
         self.browser.find_element_by_id('pic_2')
         self.browser.find_element_by_id('pic_3')
