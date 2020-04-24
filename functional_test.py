d5c5495e5e657c61588762944a1c27d7288cad12
Nattakrit Jatupattaradit <42247051+cpre20068@users.noreply.github.com>
2020-04-25 03:00:12 +0700

    Add functional test to Remove Review feature

--- functional_test.py
+++ functional_test.py
@@ -1,10 +1,11 @@
 import os
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 import time
 import unittest
 
 
-class FunctionalTest(unittest.TestCase):
+class UploadImageToGallery(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Edge("msedgedriver.exe")
 
@@ -93,6 +94,89 @@ class FunctionalTest(unittest.TestCase):
         self.browser.find_element_by_id('pic_3')
         self.browser.find_element_by_id('pic_4')
         time.sleep(2)
+
+    def test_user_can_comment_and_delete_comment_on_another_profile(self):
+        # Kitsanapong login to Match and Learn website
+        self.browser.get('http://127.0.0.1:8000/login')
+        username_box = self.browser.find_element_by_id('id_username')
+        password_box = self.browser.find_element_by_id('id_password')
+        username_box.send_keys('tongu19541')
+        password_box.send_keys("1qaz2wsx3edc4rfv5tgb")
+        login_button = self.browser.find_element_by_id('login_btn')
+        login_button.click()
+        time.sleep(2)
+        # He go to student and tutor list
+        student_and_tutor_list_btn = self.browser.find_element_by_id('student_and_tutor_list')
+        student_and_tutor_list_btn.click()
+        time.sleep(2)
+        # He click on Nattakrit profile
+        tongu20068_profile_btn = self.browser.find_element_by_id('user_tongu20068')
+        tongu20068_profile_btn.click()
+        time.sleep(2)
+        # He type "Nattakrit is a fast learner, I taught Math to him and he is understand very fast!"
+        comment_textbox = self.browser.find_element_by_id('id_comment')
+        comment_textbox.send_keys("Nattakrit is a fast learner, I taught Math to him and he is understand very fast!")
+        time.sleep(2)
+        # So, He give Nattakrit 5 star
+        self.browser.find_element_by_xpath("//select[@name='star']/option[text()='5']").click()
+        time.sleep(2)
+        # After all that, He submit comment
+        comment_btn = self.browser.find_element_by_id('comment_submit_btn')
+        comment_btn.click()
+        time.sleep(2)
+        # He saw comment he posted
+        comment_data = self.browser.find_element_by_id('comment_tongu19541').text
+        self.assertIn(comment_data,
+                      "Comment : Nattakrit is a fast learner, "
+                      "I taught Math to him and he is understand very fast!")
+        time.sleep(2)
+        # Nattakrit login to Match and Learn website
+        self.browser.get('http://127.0.0.1:8000/login')
+        username_box = self.browser.find_element_by_id('id_username')
+        password_box = self.browser.find_element_by_id('id_password')
+        username_box.send_keys('tongu20068')
+        password_box.send_keys("1qaz2wsx3edc4rfv5tgb")
+        login_button = self.browser.find_element_by_id('login_btn')
+        login_button.click()
+        time.sleep(2)
+        # Then, He go to his profile page
+        my_profile_btn = self.browser.find_element_by_id('profile_btn')
+        my_profile_btn.click()
+        time.sleep(2)
+        # He saw new comment from Kitsanapong
+        comment_data = self.browser.find_element_by_id('comment_tongu19541').text
+        self.assertIn(comment_data,
+                      "Comment : Nattakrit is a fast learner, "
+                      "I taught Math to him and he is understand very fast!")
+        time.sleep(2)
+        # Kitsanapong think he's over compliment so
+        # he login to Match and Learn website and delete his comment
+        self.browser.get('http://127.0.0.1:8000/login')
+        username_box = self.browser.find_element_by_id('id_username')
+        password_box = self.browser.find_element_by_id('id_password')
+        username_box.send_keys('tongu19541')
+        password_box.send_keys("1qaz2wsx3edc4rfv5tgb")
+        login_button = self.browser.find_element_by_id('login_btn')
+        login_button.click()
+        time.sleep(2)
+        # He go to student and tutor list
+        student_and_tutor_list_btn = self.browser.find_element_by_id('student_and_tutor_list')
+        student_and_tutor_list_btn.click()
+        time.sleep(2)
+        # He click on Nattakrit profile
+        tongu20068_profile_btn = self.browser.find_element_by_id('user_tongu20068')
+        tongu20068_profile_btn.click()
+        time.sleep(2)
+        comment_delete_btn = self.browser.find_element_by_id('delete_comment_btn_3')
+        comment_delete_btn.click()
+        time.sleep(2)
+        # He saw his comment is deleted
+        try:
+            # Find element
+            self.browser.find_element_by_id('comment_tongu19541')
+            # If not found, Pass
+        except NoSuchElementException:
+            time.sleep(2)
 
 
 if __name__ == '__main__':
